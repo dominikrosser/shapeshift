bf8a39dd38c1c8a2acfac4a7816b0a097e0b0fd4
dominikrosser <dominik.rosser@protonmail.ch>
2023-04-27 20:20:52 +0200

    Commit may be unstable.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,135 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, ecs::storage::Resources};
 use bevy_rapier2d::prelude::{*, Velocity};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy::window::PrimaryWindow;
-
-use smooth_bevy_cameras::{LookTransform, LookTransformBundle, LookTransformPlugin, Smoother};
+use bevy::input::mouse::MouseButtonInput;
+use bevy_mouse_tracking_plugin::{
+    MousePosPlugin, MousePosWorld,
+};
+use bevy::input::ButtonState;
+use particular::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
         .add_plugin(RapierDebugRenderPlugin::default())
+        .add_plugin(WorldInspectorPlugin::new())
         // Local Geomorpher Plugins:
         .add_plugin(GMCameraPlugin)
+        // Particle set
+        .add_startup_system(place_body)
+
         .add_startup_system(setup_player)
         .add_startup_system(setup_platform)
         .add_system(print_ball_altitude)
         .add_system(player_movement_system)
         .run();
 }
+
+/* PARTICULAR */
+fn place_body(
+    mut commands: Commands,
+    mut body_info: ResMut<GolfBallSettings>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    ) {
+
+    body_info.position = Some(Vec3::new(0.0, 0.0, 0.0));
+    let place_pos = body_info.position.unwrap();
+
+    let mass: f32 = 100.0_f32.max(1.0);
+    let density = 1.0;
+    let radius =
+        (mass / (density * PI)).sqrt();
+    let entity = commands.spawn(
+        CircleWithGravity {
+            shape_bundle: MaterialMesh2dBundle {
+                mesh: meshes
+                    .add(Mesh::from(shape::Circle {
+                        radius,
+                        ..default()
+                    }))
+                .into(),
+                transform: Transform::from_xyz(place_pos.x, place_pos.y, place_pos.z),
+                material: materials
+                    .add(ColorMaterial::from(Color::WHITE)),
+                    ..default()
+            },
+            collider: Collider::ball(radius),
+            friction: Friction {
+                coefficient: 10.0,
+                ..default()
+            },
+            mass: ColliderMassProperties::Mass(mass),
+            restitution: Restitution {
+                coefficient: 0.0,
+                ..default()
+            },
+            rigidbody: RigidBody::Dynamic,
+            velocity: Velocity::linear(Vec2::new(0.1,0.1)),
+            acceleration: ExternalForce::default(),
+            point_mass: PointMass::HasGravity {
+                // mass: body_info.mass,
+                mass: mass,
+            },
+        });
+}
+use std::f32::consts::PI;
+use bevy::sprite::{Material2d, MaterialMesh2dBundle};
+#[derive(Component)]
+pub enum PointMass {
+    HasGravity { mass: f32 },
+    AffectedByGravity,
+}
+
+#[derive(Bundle)]
+pub struct CircleWithGravity<M: Material2d> {
+    #[bundle]
+    pub shape_bundle: MaterialMesh2dBundle<M>,
+    pub collider: Collider,
+    pub friction: Friction,
+    pub mass: ColliderMassProperties,
+    pub restitution: Restitution,
+    pub rigidbody: RigidBody,
+    pub velocity: Velocity,
+    pub acceleration: ExternalForce,
+    pub point_mass: PointMass,
+}
+
+pub struct GolfBallSettings {
+    pub position: Option<Vec3>,
+    pub mass: f32,
+    pub trail: bool,
+}
+impl bevy::prelude::Resource for GolfBallSettings {
+    
+}
+impl Default for GolfBallSettings {
+    fn default() -> Self {
+        Self {
+            position: None,
+            mass: 20.0,
+            trail: false,
+        }
+    }
+}
+#[derive(Particle, Component)]
+struct Body {
+    position: Vec3,
+    mu: f32,
+}
+fn create_bodies(
+    mut commands: Commands,
+    ) {
+    commands.spawn(
+        Body {
+        position: Vec3::new(0.0, 0.0, 0.0),
+        mu: 1.0,
+    }
+    );
+}
+/* PARTICULAR */
 
 pub struct GMCameraPlugin;
 impl Plugin for GMCameraPlugin {
@@ -108,8 +221,6 @@ fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
     }
 }
 
-
-
 #[derive(Component)]
 pub struct Player {}
 
@@ -135,3 +246,5 @@ fn player_movement_system(
         ext_force.force = direction * 150.0;
     }
 }
+
+
